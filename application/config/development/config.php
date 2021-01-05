aede79b8e1cab96ede9e792bfc63162504dab0db
Emmanuel Campait <emmanuel.campait@vinci-it.com>
2021-01-05 06:53:52 +0100

    custom access on site

---
+++ application/config/development/config.php
@@ -0,0 +1,61 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+*/
+// Fill in the file of your project here when you develop locally.
+$root = '';
+
+// Possible hosts locally. You can add some if needed.
+$host_local = ['localhost', '127.0.0.1', '::1'];
+
+// WARNING: Do not modify the lines below
+$url = (in_array($_SERVER['HTTP_HOST'], $host_local, TRUE)) ? $_SERVER['HTTP_HOST'].'/'.$root : '';
+
+// URL to your CodeIgniter root. Typically this will be your base URL
+$config['base_url'] = ( ! empty($_SERVER['HTTPS'])) ? 'https://'.$url.'/' : 'http://'.$url.'/';
+
+/*
+|--------------------------------------------------------------------------
+| Error Logging Threshold
+|--------------------------------------------------------------------------
+|
+|	0 = Disables logging, Error logging TURNED OFF
+|	1 = Error Messages (including PHP errors)
+|	2 = Debug Messages
+|	3 = Informational Messages
+|	4 = All Messages
+|
+*/
+$config['log_threshold'] = 1;
+
+/*
+|--------------------------------------------------------------------------
+| Session Variables
+|--------------------------------------------------------------------------
+|
+*/
+$config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'gas_dev';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = APPPATH.'sessions/';
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
+
+/*
+|--------------------------------------------------------------------------
+| Cross Site Request Forgery
+|--------------------------------------------------------------------------
+|
+*/
+$config['csrf_protection'] = TRUE;
+$config['csrf_token_name'] = 'csrf_t_gas_dev';
+$config['csrf_cookie_name'] = 'csrf_c_gas_dev';
+$config['csrf_expire'] = 7200;
+$config['csrf_regenerate'] = TRUE;
+$config['csrf_exclude_uris'] = [];
